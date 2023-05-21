f971a1778b6e7345b0b24f61fe89dd517c81cd6a
jimmy <jimmy.galland@celjim.fr>
2023-05-21 02:13:56 +0200

    add translate

--- files/board/arpl/overlayfs/opt/arpl/install-addons.sh
+++ files/board/arpl/overlayfs/opt/arpl/install-addons.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
-
+set -e
 . /opt/arpl/include/functions.sh
 
-set -e
-
 # Detect if has new local plugins to install/reinstall
-for F in `ls ${CACHE_PATH}/*.addon 2>/dev/null`; do
-  ADDON=`basename "${F}" | sed 's|.addon||'`
+for F in $(ls ${CACHE_PATH}/*.addon 2>/dev/null); do
+  ADDON=$(basename "${F}" | sed 's|.addon||')
   rm -rf "${ADDONS_PATH}/${ADDON}"
   mkdir -p "${ADDONS_PATH}/${ADDON}"
   echo "Installing ${F} to ${ADDONS_PATH}/${ADDON}"
