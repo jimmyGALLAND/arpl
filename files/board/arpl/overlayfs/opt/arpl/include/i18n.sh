fd8f2dfc3aead861822b6733352daaf6ba34460e
jimmy <jimmy.galland@celjim.fr>
2023-05-21 22:00:30 +0200

    fix code_lang [200~i18n.sh

--- files/board/arpl/overlayfs/opt/arpl/include/i18n.sh
+++ files/board/arpl/overlayfs/opt/arpl/include/i18n.sh
@@ -4,7 +4,7 @@ fi
 
 #codes_lang=(en_US ar_YE ca_ES de_DE el_GR es_ES es_MX eu_ES fr_FR hu_HU it_IT ja_JP ko_KR nb_NO nl_NL nn_NO oc_FR pl_PL pt_BR pt_PT ru_RU uk_UA sv_SE tr_TR zh_CN zh_TW)
 
-#codes_lang=(en_US fr_FR zh_CN)
+codes_lang=(en_US fr_FR zh_CN)
 
 declare -A available_locales
 export available_locales
